991975bdfc1a291a5dcd1810c60b9f777a2bb702
Kacper Mila <77899626+Kacper-Mila@users.noreply.github.com>
2023-01-17 10:49:40 +0100

    Initial Commit

--- src/main/java/com/project/w3t/W3tApplication.java
+++ src/main/java/com/project/w3t/W3tApplication.java
@@ -2,6 +2,8 @@ package com.project.w3t;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class W3tApplication {
@@ -9,5 +11,4 @@ public class W3tApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(W3tApplication.class, args);
 	}
-
 }
