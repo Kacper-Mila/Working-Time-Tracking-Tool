0b0db565896de7bbb7511ce4d064b30ba873ea8e
Kacper Mila <77899626+Kacper-Mila@users.noreply.github.com>
2023-01-17 10:21:57 +0100

    Initial Commit

---
+++ src/main/java/com/czlowieki/w3t/W3tApplication.java
@@ -0,0 +1,13 @@
+package com.czlowieki.w3t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3tApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W3tApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czlowieki/w3t/W3tApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czlowieki.w3t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W3tApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
