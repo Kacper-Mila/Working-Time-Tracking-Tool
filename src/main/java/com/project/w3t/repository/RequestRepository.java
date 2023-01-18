9566d8cdc4a4e1b8addd3ceae22905447f760e1d
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-01-18 21:04:27 +0100

    updateRequest method added

--- src/main/java/com/project/w3t/repository/RequestRepository.java
+++ src/main/java/com/project/w3t/repository/RequestRepository.java
@@ -2,10 +2,14 @@ package com.project.w3t.repository;
 
 import com.project.w3t.exceptions.InvalidCommentLength;
 import com.project.w3t.exceptions.InvalidDateRangeException;
+import com.project.w3t.exceptions.InvalidRequestId;
 import com.project.w3t.model.Request;
+import com.project.w3t.model.RequestDto;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RequestRepository {
     void addRequest(Request request) throws InvalidDateRangeException, InvalidCommentLength;
+
+    void updateRequest(Long id, RequestDto requestDto) throws InvalidRequestId;
 }
