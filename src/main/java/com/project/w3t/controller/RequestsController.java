13f3bc27cb384da89677e991e0a3a47fd8e191c8
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-01-18 13:30:43 +0100

    RequestMapping path updated

--- src/main/java/com/project/w3t/controller/RequestsController.java
+++ src/main/java/com/project/w3t/controller/RequestsController.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/requests")
+@RequestMapping("api/v1/requests")
 public class RequestsController {
 
     @Autowired
