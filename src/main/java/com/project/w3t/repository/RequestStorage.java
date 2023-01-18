e3354fbd858bea28b3b136c278c38bbac9aeebbf
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-01-18 13:20:49 +0100

    checkDateRange, checkCommentLength and addRequest methods implemented

--- src/main/java/com/project/w3t/repository/RequestStorage.java
+++ src/main/java/com/project/w3t/repository/RequestStorage.java
@@ -1,11 +1,12 @@
 package com.project.w3t.repository;
 
+import com.project.w3t.exceptions.InvalidCommentLength;
+import com.project.w3t.exceptions.InvalidDateRangeException;
 import com.project.w3t.model.Request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +18,29 @@ import java.util.List;
 public class RequestStorage {
 
     private List<Request> userRequestList = new ArrayList<>();
+    private static int COMMENT_MAX_LENGTH = 250;
 
-    public void addRequest(Request request) {
-        userRequestList.add(request);
+    public void addRequest(Request request) throws InvalidDateRangeException, InvalidCommentLength {
+        if (!checkDateRange(request)) {
+            if (checkCommentLength(request)) {
+                userRequestList.add(request);
+            } else {
+                throw new InvalidCommentLength();
+            }
+        } else {
+            throw new InvalidDateRangeException();
+        }
     }
+
+    public boolean checkDateRange(Request request) {
+        return userRequestList.stream()
+                .filter(req -> req.getType().equals(request.getType()))
+                .anyMatch(req -> (req.getRequestDateRange().contains(request.getStartDate())
+                        || (req.getRequestDateRange().contains(request.getEndDate()))));
+    }
+
+    public boolean checkCommentLength(Request request) {
+        return request.getComment().length() <= COMMENT_MAX_LENGTH;
+    }
+
 }
