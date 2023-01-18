add5e9687f88d2529fe0da0467fbd7132b52f6e7
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-01-18 13:23:24 +0100

    POST endpoint for adding request added

--- src/main/java/com/project/w3t/W3tApplication.java
+++ src/main/java/com/project/w3t/W3tApplication.java
@@ -2,13 +2,11 @@ package com.project.w3t;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class W3tApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(W3tApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(W3tApplication.class, args);
+    }
 }
