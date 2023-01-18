a5dec41f5b7be557ce59961fbbc52dafcb7d1ef7
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-01-18 21:04:15 +0100

    updateRequest method implemented

--- src/main/java/com/project/w3t/service/RequestService.java
+++ src/main/java/com/project/w3t/service/RequestService.java
@@ -2,7 +2,9 @@ package com.project.w3t.service;
 
 import com.project.w3t.exceptions.InvalidCommentLength;
 import com.project.w3t.exceptions.InvalidDateRangeException;
+import com.project.w3t.exceptions.InvalidRequestId;
 import com.project.w3t.model.Request;
+import com.project.w3t.model.RequestDto;
 import com.project.w3t.repository.RequestRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,7 @@ public class RequestService {
         this.requestRepository = requestRepository;
     }
 
-    public void addRequest(Request request){
+    public void addRequest(Request request) {
         try {
             requestRepository.addRequest(request);
         } catch (InvalidDateRangeException e) {
@@ -22,7 +24,14 @@ public class RequestService {
         } catch (InvalidCommentLength e) {
             System.out.println("Your comment is too long!");
         }
+    }
 
+    public void updateRequest(Long id, RequestDto requestDto) {
+        try {
+            requestRepository.updateRequest(id, requestDto);
+        } catch (InvalidRequestId e) {
+            System.out.println("Invalid request Id!");
+        }
     }
 
 
