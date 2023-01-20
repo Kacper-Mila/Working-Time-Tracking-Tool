864190725eec27d59495d576d825f9402d83ec35
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-01-20 11:27:09 +0100

    addRequest and updateRequest methods refactor

--- src/main/java/com/project/w3t/service/RequestService.java
+++ src/main/java/com/project/w3t/service/RequestService.java
@@ -1,16 +1,14 @@
 package com.project.w3t.service;
 
 import com.project.w3t.exceptions.InvalidCommentLengthException;
-import com.project.w3t.exceptions.InvalidDateRangeException;
 import com.project.w3t.exceptions.InvalidRequestIdException;
-import com.project.w3t.repository.RequestRepository;
-import org.springframework.stereotype.Service;
 import com.project.w3t.model.request.Request;
 import com.project.w3t.model.request.RequestDto;
+import com.project.w3t.repository.RequestRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class RequestService {
@@ -24,34 +22,20 @@ public class RequestService {
         return requestRepository.getAllRequests();
     }
 
-    public void addRequest(Request request) {
-        try {
-            requestRepository.addRequest(request);
-        } catch (InvalidDateRangeException e) {
-            System.out.println("Invalid date range!");
-        } catch (InvalidCommentLengthException e) {
-            System.out.println("Your comment is too long!");
-        }
+    public void addRequest(Request request) throws InvalidCommentLengthException {
+        requestRepository.addRequest(request);
     }
 
-    public void updateRequest(Long id, RequestDto requestDto) {
-        try {
-            requestRepository.updateRequest(id, requestDto);
-        } catch (InvalidRequestIdException e) {
-            System.out.println("Invalid request Id!");
-        } catch (InvalidCommentLengthException e) {
-            System.out.println("Your comment is too long!");
-        } catch (InvalidDateRangeException e) {
-            System.out.println("Invalid date range!");
-        }
+    public void updateRequest(Long id, RequestDto requestDto) throws InvalidCommentLengthException, InvalidRequestIdException {
+        requestRepository.updateRequest(id, requestDto);
     }
 
     public void deleteRequest(Long requestId) throws InvalidRequestIdException {
-            requestRepository.deleteRequest(requestId);
+        requestRepository.deleteRequest(requestId);
     }
 
     public List<Request> getAllRequestsByType(String requestType) throws NullPointerException {
-            return requestRepository.getAllRequestsByType(requestType);
+        return requestRepository.getAllRequestsByType(requestType);
     }
 
     public Optional<Request> getRequestById(Long requestId) {
