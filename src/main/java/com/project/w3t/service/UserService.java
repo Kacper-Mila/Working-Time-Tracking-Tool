26c9395343ea7b780e13f951530f097351ce5aaa
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-02-02 22:41:36 +0100

    updateUser method added

--- src/main/java/com/project/w3t/service/UserService.java
+++ src/main/java/com/project/w3t/service/UserService.java
@@ -4,6 +4,7 @@ import com.project.w3t.exceptions.BadRequest400.BadRequestException;
 import com.project.w3t.exceptions.NotFound404.NotFoundException;
 import com.project.w3t.model.user.User;
 import com.project.w3t.model.user.UserDto;
+import com.project.w3t.model.user.UserType;
 import com.project.w3t.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -34,19 +35,39 @@ public class UserService {
         if (testId.isPresent() && userRepository.existsById(testId.get())) user.setId(null);
 
 //        TODO throw custom exc - email/user already taken, validation elsewhere (f.ex. email: string + @ + string + . + string).
-        if (userRepository.existsByEmail(user.getEmail())) throw new BadRequestException("Unable to process request - email address is invalid.");
-        if (userRepository.existsByUserId(user.getUserId())) throw new BadRequestException("Unable to process request - user Id is invalid.");
+        if (userRepository.existsByEmail(user.getEmail()))
+            throw new BadRequestException("Unable to process request - email address is invalid.");
+        if (userRepository.existsByUserId(user.getUserId()))
+            throw new BadRequestException("Unable to process request - user Id is invalid.");
         userRepository.save(user);
     }
-
     public void updateUser(String userId, UserDto userDto) {
-//        TODO implement
+        User userToUpdate = getUserByUserId(userId);
+
+        if (!isUserTypeValid(userDto.getUserType())) {
+            throw new BadRequestException("Invalid user type.");
+        }
+        if (userDto.getManagerId() == null || userDto.getManagerId().isBlank()) {
+            throw new BadRequestException("Manager id is missing");
+        }
+        if (userDto.getTeamId() == null || userDto.getTeamId().isBlank()) {
+            throw new BadRequestException("Team id is missing");
+        }
+        userToUpdate.setUserType(userDto.getUserType());
+        userToUpdate.setManagerId(userDto.getManagerId());
+        userToUpdate.setTeamId(userDto.getTeamId());
+        userRepository.save(userToUpdate);
+    }
+
+    private boolean isUserTypeValid(UserType userType) {
+        return userType != null;
     }
 
     @Transactional
     public void deleteUser(String userId) {
 //        TODO proper exception and status code
-        if (!userRepository.existsByUserId(userId)) throw new NotFoundException("Unable to process request - user does not exist.");
+        if (!userRepository.existsByUserId(userId))
+            throw new NotFoundException("Unable to process request - user does not exist.");
         userRepository.deleteByUserId(userId);
     }
 
@@ -60,7 +81,8 @@ public class UserService {
 
     public User getUserByUserId(String userId) {
         //        TODO proper exception and status code
-        if (!userRepository.existsByUserId(userId)) throw new NotFoundException("Unable to process request - user does not exist.");
+        if (!userRepository.existsByUserId(userId))
+            throw new NotFoundException("Unable to process request - user does not exist.");
         return userRepository.findByUserId(userId);
     }
 }
