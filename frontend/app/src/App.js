3f014e6842680a8430c36835f5c042d2fccb03e4
Bartosz Jan Wolak <bartosz.wolak@motorolasolutions.com>
2023-02-16 10:19:07 +0100

    Pre branching commit.

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -14,7 +14,7 @@ export default function App() {
         </div>
     );
 }
-//
+
 // return (
 //     <div>
 //         <Navbar />
--- frontend/app/src/pages/MainPage.js
+++ frontend/app/src/pages/MainPage.js
@@ -1,9 +1,27 @@
 import User from "../components/User";
+import {Route, Routes} from "react-router-dom";
+import AppNavbar from "../components/navbar/AppNavbar";
+import Calendar from "../components/Calendar";
+import Requests from "./Requests";
 
 export default function MainPage() {
     return (
         <div>
-            <User/>
+            <AppNavbar/>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-lg-3 col-md-3 col-sm-12 border' id='user-left'>
+                        <User/>
+                    </div>
+
+                    <div className='col-lg-9 col-md-9 col-sm-12 border' id='pages'>
+                        <Routes>
+                            <Route path='/' element={<Calendar/>}></Route>
+                            <Route path='/requests' element={<Requests/>}></Route>
+                        </Routes>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
