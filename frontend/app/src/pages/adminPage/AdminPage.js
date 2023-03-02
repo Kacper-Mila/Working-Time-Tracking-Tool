9b45635c0ae90f6cee4f2cdde52e7e23cbcfd901
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-03-02 14:28:25 +0100

    user filter for admin added

--- frontend/app/src/pages/adminPage/AdminPage.js
+++ frontend/app/src/pages/adminPage/AdminPage.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AdminHeader from "../../layouts/users/AdminHeader";
 import Users from "../../layouts/users/Users";
 
 
 export default function AdminPage() {
+    const [selectedUserType, setSelectedUserType] = useState("ALL");
     return (
         <div>
-            <AdminHeader/>
+            <AdminHeader setSelectedUserType={setSelectedUserType}/>
             <div className='user-container'>
-                <Users/>
+                <Users userType={selectedUserType}/>
             </div>
         </div>
     );
