bcb2e7ed7e239ed9e53e0333c48bc69104548c0d
CWTB47 <anna.pancerz@motorolasolutions.com>
2023-03-02 13:54:37 +0100

    edit user for admin implemented

---
+++ frontend/app/src/layouts/users/EditUser.js
@@ -0,0 +1,138 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+export default function EditUser(props){
+    const [userType, setUserType] = useState('');
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [password, setPassword] = useState('');
+    const [managerId, setManagerId] = useState('');
+    const [teamId, setTeamId] = useState('');
+    const [holidays, setHolidays] = useState('');
+
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        console.log(props.userId);
+
+        await axios.patch(`http://localhost:8080/api/v1/users/update?userId=${props.userId}`, {
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: password,
+            holidays: holidays,
+            userType: userType,
+            managerId: managerId,
+            teamId: teamId,
+        }).catch(err => {
+            let response = JSON.parse(err.request.response);
+            alert(response.message);
+        });
+        props.onCancel();
+        window.location.reload();
+    }
+
+    return (
+        <div>
+            <div className="container py-5 h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                        <form className='pb-5' onSubmit={onSubmit}>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="form1Example13">First
+                                    Name </label>
+                                <input type="text"
+                                       className="form-control form-control-lg"
+                                       value={firstName}
+                                       onChange={(e) => setFirstName(e.target.value)}
+                                       required
+                                />
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="form1Example13">Last Name </label>
+                                <input type="text"
+                                       className="form-control form-control-lg"
+                                       value={lastName}
+                                       onChange={(e) => setLastName(e.target.value)}
+                                       required
+                                />
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="form1Example13">Email </label>
+                                <input type="email"
+                                       className="form-control form-control-lg"
+                                       value={email}
+                                       onChange={(e) => setEmail(e.target.value)}
+                                       required
+                                />
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="form1Example13">Password </label>
+                                <input type="password"
+                                       className="form-control form-control-lg"
+                                       value={password}
+                                       onChange={(e) => setPassword(e.target.value)}
+                                       required
+                                />
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="form1Example13"> User
+                                    Type </label>
+                                <select className="form-control form-control-lg"
+                                        value={userType}
+                                        onChange={(e) => setUserType(e.target.value)}
+                                        required style={{cursor: "pointer"}}>
+                                    <option>SELECT</option>
+                                    <option>EMPLOYEE</option>
+                                    <option>MANAGER</option>
+                                    <option>ADMIN</option>
+
+                                </select>
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="form1Example13">Team ID </label>
+                                <input type="text"
+                                       className="form-control form-control-lg"
+                                       value={teamId}
+                                       onChange={(e) => setTeamId(e.target.value)}
+                                       required
+                                />
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="form1Example13">Manager
+                                    ID </label>
+                                <input type="text"
+                                       className="form-control form-control-lg"
+                                       value={managerId}
+                                       onChange={(e) => setManagerId(e.target.value)}
+                                       required
+                                />
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="form1Example13">Holidays </label>
+                                <input type="number"
+                                       className="form-control form-control-lg"
+                                       value={holidays}
+                                       onChange={(e) => setHolidays(e.target.value)}
+                                       required
+                                />
+                            </div>
+
+                            <div className="d-flex mb-4"></div>
+                            <button type="submit"
+                                    className="btn mr-4 btn-outline-success btn-lg btn-block"
+                            >Save
+                            </button>
+                            <button type="reset"
+                                    className="btn mr-4 btn-outline-danger btn-lg btn-block"
+                                    onClick={props.onCancel}
+                            >Cancel
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
