374bc43132e0a243c126d6949c0aea4ef00b0184
Dariusz Gaczyński <74984745+dariuszgaczynski@users.noreply.github.com>
2023-03-03 09:33:16 +0100

    commit 4 Bartek

--- src/main/java/com/project/w3t/security/config/SecurityConfig.java
+++ src/main/java/com/project/w3t/security/config/SecurityConfig.java
@@ -14,8 +14,6 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
